b4356e4d9099de87b60b2c92d891bac092feb9e2
Hotai04 <bendadavide@gmail.com>
2018-07-06 10:33:22 +0200

    Esame 26_01

---
+++ Soprana-jan26-master/app.js
@@ -0,0 +1,29 @@
+
+const express = require('express'),
+    bodyParser = require('body-parser'),
+	check = require('./checker.js');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.set('port', (process.env.PORT || 5000));
+
+
+
+
+// a useless function that returns a fixed object. you can use it, if you want, for testing purposes
+app.get('/count',function (req, res) {
+    res.json({count: 3})
+})
+
+app.post('/check',(req,res) => {
+	check(req.body.url,req.body.invocationParameters,req.body.expectedResultData,req.body.expectedResultStatus)
+		.then(result => {
+			res.status(200).json(result);
+		});
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
